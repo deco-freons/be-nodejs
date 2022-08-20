a7738faec38e17697b12d543bc73e8818ca549a0
Surya Nirvana <suryanirvanaprima@gmail.com>
2022-08-20 18:08:59 +1000

    feat: :sparkles: implement read and update user details

--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -8,7 +8,10 @@ import User from '../../auth/entity/user.entity';
 
 import Preference from '../entity/preference.entity';
 import UserPreferenceDTO from '../dto/user.preference.dto';
-import { UpsertUserPreferenceResponseLocals } from '../response/upsertUserPreference.response';
+import UpdateUserDTO from '../dto/user.update.dto';
+import { UpsertUserPreferenceResponseLocals } from '../response/userPreference.upsert.response';
+import { ReadUserResponseLocals } from '../response/user.read.response';
+import { UpdateUserResponseLocals } from '../response/user.update.response';
 
 class UserService implements BaseService {
     userRepository: Repository<ObjectLiteral>;
@@ -26,9 +29,9 @@ class UserService implements BaseService {
             const user = await this.getUserByEmailAndUsername(email, username);
             if (!user) throw new NotFoundException('User does not exist.');
 
-            const preferenceID = body.preferenceID;
-            const preferences = await this.getPreferences(preferenceID);
-            if (!preferences) throw new BadRequestException('Preferences Invalid');
+            const preferenceIDs = body.preferences;
+            const preferences = await this.getPreferencesByID(preferenceIDs);
+            if (!preferences) throw new BadRequestException('Preferences Invalid.');
 
             await this.updateUserPreferences(user, preferences);
 
@@ -38,7 +41,61 @@ class UserService implements BaseService {
         }
     };
 
-    private getPreferences = async (preferenceIDs: string[]) => {
+    public readUser = async (locals: ReadUserResponseLocals) => {
+        try {
+            const email = locals.email;
+            const username = locals.username;
+            const user = await this.getUserByEmailAndUsername(email, username);
+            if (!user) throw new NotFoundException('User does not exist.');
+
+            const preferences = await this.getUserPreferences(user);
+            const userData = this.constructUserData(user, preferences);
+
+            return { message: 'Successfully retrieved user details.', userData: userData };
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    public updateUser = async (body: UpdateUserDTO, locals: UpdateUserResponseLocals) => {
+        try {
+            const email = locals.email;
+            const username = locals.username;
+            await this.updateUserDetails(body, email, username);
+
+            const user = await this.getUserByEmailAndUsername(email, username);
+            if (!user) throw new NotFoundException('User does not exist.');
+
+            const preferenceIDs = body.preferences;
+            const preferences = await this.getPreferencesByID(preferenceIDs);
+            if (!preferences) throw new BadRequestException('Preferences Invalid.');
+
+            await this.updateUserPreferences(user, preferences);
+
+            const userData = this.constructUserData(user, preferences);
+
+            return { message: 'Successfully update user details', userData: userData };
+        } catch (error) {
+            throw error;
+        }
+    };
+
+    private constructUserData = (user: User, preferences: Preference[]) => {
+        const userData: Partial<User> = {
+            userID: user.userID,
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            birthDate: user.birthDate,
+            preferences: preferences,
+            isVerified: user.isVerified,
+            isFirstLogin: user.isFirstLogin,
+        };
+        return userData;
+    };
+
+    private getPreferencesByID = async (preferenceIDs: string[]) => {
         const queryBuilder = this.preferenceRepository.createQueryBuilder();
         const preferences = await queryBuilder
             .select(['preference.preferenceID', 'preference.preferenceName'])
@@ -51,7 +108,17 @@ class UserService implements BaseService {
     private getUserByEmailAndUsername = async (email: string, username: string) => {
         const queryBuilder = this.userRepository.createQueryBuilder();
         const user = await queryBuilder
-            .select(['user.userID', 'user.username', 'user.email', 'user.isVerified'])
+            .select([
+                'user.userID',
+                'user.username',
+                'user.firstName',
+                'user.lastName',
+                'user.email',
+                'user.password',
+                'user.birthDate',
+                'user.isVerified',
+                'user.isFirstLogin',
+            ])
             .from(User, 'user')
             .where('user.email = :email', { email: email })
             .andWhere('user.username = :username', { username: username })
@@ -59,10 +126,26 @@ class UserService implements BaseService {
         return user;
     };
 
+    private getUserPreferences = async (user: User) => {
+        const queryBuilder = this.userRepository.createQueryBuilder();
+        const preferences = await queryBuilder.relation(User, 'preferences').of(user).loadMany();
+        return preferences;
+    };
+
     private updateUserPreferences = async (user: User, preferences: Preference[]) => {
         const queryBuilder = this.userRepository.createQueryBuilder();
-        const oldPreferences = await queryBuilder.relation(User, 'preferences').of(user).loadMany();
+        const oldPreferences = await this.getUserPreferences(user);
         await queryBuilder.relation(User, 'preferences').of(user).addAndRemove(preferences, oldPreferences);
+    };
+
+    private updateUserDetails = async (body: UpdateUserDTO, email: string, username: string) => {
+        const queryBuilder = this.userRepository.createQueryBuilder();
+        await queryBuilder
+            .update(User)
+            .set({ firstName: body.firstName, lastName: body.lastName, birthDate: body.birthDate })
+            .where('username = :username', { username: username })
+            .andWhere('email = :email', { email: email })
+            .execute();
     };
 }
 
