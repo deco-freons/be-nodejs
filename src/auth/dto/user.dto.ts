76de8b7f9ed2c76015da9e277408bbdfb5ca807d
Surya Nirvana <suryanirvanaprima@gmail.com>
2022-10-01 10:49:48 +1000

    feat: :sparkles: return participants list

--- src/auth/dto/user.dto.ts
+++ src/auth/dto/user.dto.ts
@@ -1,6 +1,7 @@
-import EventDetails from '../../event/entity/event.details.entity';
 import Location from '../../common/entity/location.entity';
 import Preference from '../../common/entity/preference.entity';
+import EventDetails from '../../event/entity/event.details.entity';
+import Image from '../../image/entity/image.entity';
 
 class UserDTO {
     userID: number;
@@ -10,6 +11,7 @@ class UserDTO {
     email: string;
     birthDate: string;
     location: Partial<Location>;
+    userImage: Partial<Image>;
     preferences: Preference[];
     eventCreated: Partial<EventDetails>[];
     isVerified: boolean;
