e7b9a020834fadbead559ad5eb505a31597156d8
Surya Nirvana <41690774+suryanirvana@users.noreply.github.com>
2022-10-16 14:20:56 +1000

    fix: :bug: fix email html and email image (#46)

--- src/common/enum/email.enum.ts
+++ src/common/enum/email.enum.ts
@@ -1,6 +1,7 @@
 export enum EMAIL {
+    IMAGE = '/../../common/mail/logo.png',
     VERIFY = '/../../common/mail/verify.html',
-    VERIFY_SUBJECT = 'Verify your _ Account',
+    VERIFY_SUBJECT = 'Verify your gatherly Account',
     FORGET_PASSWORD = '/../../common/mail/forget-password.html',
-    FORGET_PASSWORD_SUBJECT = 'Forget Password Instructions for _',
+    FORGET_PASSWORD_SUBJECT = 'Forget Password Instructions for your gatherly Account',
 }
