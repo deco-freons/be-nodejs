3b3523dfbdd7cf8e7131a6fea4d8fa5959edf56f
Surya Nirvana <41690774+suryanirvana@users.noreply.github.com>
2022-10-16 12:27:48 +1000

    docs: :memo: add data to import (#45)

---
+++ src/data/preference.sql
@@ -0,0 +1,8 @@
+insert into public.preference (preference_id, preference_name)
+values  ('GM', '🎮 Gaming'),
+        ('MV', '🎬 Movie'),
+        ('DC', '💃️ Dancing'),
+        ('CL', '🍽 Culinary'),
+        ('BB', '🏀 Basketball'),
+        ('NT', '🍃️ Nature'),
+        ('FB', '⚽️ Football');
